1bd26e0bdfb944a56548667f939137163ad553be
g0sy23 <92913145+g0sy23@users.noreply.github.com>
2023-04-10 07:14:04 +0600

    Refactor button, endpoint, turtle blueprints

--- Source/MyTest/Private/MyAIBlueprintHelperLibrary.cpp
+++ Source/MyTest/Private/MyAIBlueprintHelperLibrary.cpp
@@ -3,7 +3,35 @@
 
 #include "MyAIBlueprintHelperLibrary.h"
 
+#include "AIController.h"
+#include "Navigation/PathFollowingComponent.h"
 #include "TimerManager.h"
+
+namespace
+{
+	UPathFollowingComponent* InitNavigationControl(AController& Controller)
+	{
+		AAIController* AsAIController = Cast<AAIController>(&Controller);
+		UPathFollowingComponent* PathFollowingComp = nullptr;
+
+		if (AsAIController)
+		{
+			PathFollowingComp = AsAIController->GetPathFollowingComponent();
+		}
+		else
+		{
+			PathFollowingComp = Controller.FindComponentByClass<UPathFollowingComponent>();
+			if (PathFollowingComp == nullptr)
+			{
+				PathFollowingComp = NewObject<UPathFollowingComponent>(&Controller);
+				PathFollowingComp->RegisterComponentWithWorld(Controller.GetWorld());
+				PathFollowingComp->Initialize();
+			}
+		}
+
+		return PathFollowingComp;
+	}
+}
 
 void UMyAIBlueprintHelperLibrary::MySimpleMoveToLocation1(AController* Controller, const FVector& Goal)
 {
@@ -19,6 +47,36 @@ void UMyAIBlueprintHelperLibrary::MySimpleMoveToLocation2(AController* Controlle
 
 	FTimerHandle timerHandle1;
 	timerManager.SetTimer(timerHandle1, [=]() { Controller->StopMovement(); }, 1.5f, true, 1.0f);
+
+	FTimerHandle timerHandle2;
+	timerManager.SetTimer
+	(
+		timerHandle2,
+		[=]() mutable
+		{
+			if (Controller == nullptr)
+			{
+				return;
+			}
+
+			if (!Controller->GetWorld()->GetTimerManager().TimerExists(timerHandle) && !Controller->GetWorld()->GetTimerManager().TimerExists(timerHandle))
+			{
+				return;
+			}
+
+			UPathFollowingComponent* PFollowComp = InitNavigationControl(*Controller);
+			const bool bAlreadyAtGoal = PFollowComp->HasReached(Goal, EPathFollowingReachMode::OverlapAgent);
+			if (bAlreadyAtGoal || Controller->GetPawn() == nullptr)
+			{
+				UAIBlueprintHelperLibrary::SimpleMoveToLocation(Controller, Goal);
+				Controller->GetWorld()->GetTimerManager().ClearTimer(timerHandle);
+				Controller->GetWorld()->GetTimerManager().ClearTimer(timerHandle1);
+			}
+		},
+		0.5f,
+		true,
+		0.0f
+	);
 }
 
 void UMyAIBlueprintHelperLibrary::MySimpleMoveToLocation3(AController* Controller, const FVector& Goal, const FVector& Goal1)
@@ -30,4 +88,34 @@ void UMyAIBlueprintHelperLibrary::MySimpleMoveToLocation3(AController* Controlle
 
 	FTimerHandle timerHandle1;
 	timerManager.SetTimer(timerHandle1, [=]() { UAIBlueprintHelperLibrary::SimpleMoveToLocation(Controller, Goal1); }, 1.5f, true, 1.0f);
+
+	FTimerHandle timerHandle2;
+	timerManager.SetTimer
+	(
+		timerHandle2,
+		[=]() mutable
+		{
+			if (Controller == nullptr)
+			{
+				return;
+			}
+
+			if (!Controller->GetWorld()->GetTimerManager().TimerExists(timerHandle) && !Controller->GetWorld()->GetTimerManager().TimerExists(timerHandle))
+			{
+				return;
+			}
+
+			UPathFollowingComponent* PFollowComp = InitNavigationControl(*Controller);
+			const bool bAlreadyAtGoal = PFollowComp->HasReached(Goal, EPathFollowingReachMode::OverlapAgent);
+			if (bAlreadyAtGoal || Controller->GetPawn() == nullptr)
+			{
+				UAIBlueprintHelperLibrary::SimpleMoveToLocation(Controller, Goal);
+				Controller->GetWorld()->GetTimerManager().ClearTimer(timerHandle);
+				Controller->GetWorld()->GetTimerManager().ClearTimer(timerHandle1);
+			}
+		},
+		0.5f,
+		true,
+		0.0f
+	);
 }
